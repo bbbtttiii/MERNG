0f83468c1c566383d7c4649df5d2c4948853203b
bbbtttiii <ben.iburg@gmail.com>
2021-01-12 19:27:16 -0600

    add PostCard

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Card, Icon, Label, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+export default function PostCard({ props: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow()}</Card.Meta>
+        <Card.Description>
+          {body}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <p>Buttons</p>
+      </Card.Content>
+    </Card>
+  )
+}
